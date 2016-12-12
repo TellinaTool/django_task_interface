32d2844c597700a3bafb39da9ec57b54a46bde1a
Kevin Vu <kevin.m.vu@gmail.com>
2016-12-12 00:28:42 -0800

    Update Bash script to exit on empty argument.

--- delete_filesystem.bash
+++ delete_filesystem.bash
@@ -2,6 +2,13 @@
 
 name=$1
 
+# Exit on empty name
+if [[ -z "${name// }" ]]; then
+	exit 1
+fi
+
+set +e # ignore error from umount, if FS is already unmounted
 umount -f /$name
+set -e
 rm -rf ~/$name.ext4
 rm -rf /$name
--- make_filesystem.bash
+++ make_filesystem.bash
@@ -2,9 +2,14 @@
 
 name=$1
 
+# Exit on empty name
+if [[ -z "${name// }" ]]; then
+	exit 1
+fi
+
 mkdir /$name
 dd if=/dev/zero of=~/$name.ext4 count=20480 status=none
 mkfs.ext4 -q -F ~/$name.ext4
 mount -o loop,rw ~/$name.ext4 /$name
 mkdir /$name/home
-chown --recursive vagrant:vagrant /$name/home
+chown --recursive kvu:kvu /$name/home
