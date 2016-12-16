9e4f07469a09321042509870630ca99a2658aaed
Kevin Vu <kevin.m.vu@gmail.com>
2016-12-16 14:31:30 -0800

    Clarify documentation

--- delete_filesystem.bash
+++ delete_filesystem.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # usage: delete_filesystem.bash [name]
-# This script deletes the virtual filesystem mounted at /name
+# This script deletes the virtual filesystem mounted at /name.
+# This is meant to be run on the task interface host.
 
 name=$1
 
