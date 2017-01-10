c26cf849509c1f17032ae9194d691924d74fed30
Victoria X. Lin <xilin@cs.washington.edu>
2017-01-10 02:13:14 -0800

    Update make_filesystem.bash

--- make_filesystem.bash
+++ make_filesystem.bash
@@ -2,9 +2,11 @@
 
 # usage: make_filesystem.bash [name]
 # This script creates a virtual filesystem and mounts it at /name.
+# It also copies the pre-defined filesystem in the data folder to /name/home/.
 # This is meant to be run on the task interface host.
 
 name=$1
+fs_path=$2
 
 # Exit on empty name
 if [[ -z "${name// }" ]]; then
@@ -16,4 +18,6 @@ dd if=/dev/zero of=~/$name.ext4 count=20480
 mkfs.ext4 -q -F ~/$name.ext4
 mount -o loop,rw ~/$name.ext4 /$name
 mkdir /$name/home
+# Copy file system
+cp -r $fs_path /$name/home
 chown --recursive vagrant:vagrant /$name/home
