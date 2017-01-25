ad25a3bb3ef53978090d624b0ee77489a3a46807
Chenglong <mestway@gmail.com>
2017-01-25 15:14:00 -0800

    panel size is now auto adjust

--- website/static/js/task.js
+++ website/static/js/task.js
@@ -233,6 +233,9 @@ $(document).ready(function () {
             // reset height of file system diff and stdout diff
             //$("#task-progress-container").show();
             //$("#current-tree-vis-container").css('bottom', '50%');
+            
+            $("#current-tree-vis-container").css("bottom", "70%");
+            $("#task-progress-vis-container").css("top", "30%");
 
             $("#task-progress-report").append('<li><font style="text-decoration: underline;">Your terminal output mismatches the solution output, see below</font>: \
                                                    <ul class="legend-ul"><li>There are lines missing in your print result: <span style="color:#CCCCCC">an output line</span>.</li> \
