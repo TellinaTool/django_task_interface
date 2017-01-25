2cec52cef9ee2d739468dc3b63a086105205a1b4
Chenglong <mestway@gmail.com>
2017-01-25 15:53:04 -0800

    fix introJS target problem

--- website/static/js/training.js
+++ website/static/js/training.js
@@ -14,32 +14,32 @@ var fs_search_training = {
             position: "right"
         },
         {
-            element: "#bash-terminal",
+            element: "#bash-container",
             intro: "Execute your commands in the bash terminal. The command may solve the task or explore the file system.  If the command solves the task, you will proceed to the next task.",
             position: "right"
         },
         {
-            element: "#current-tree-vis",
+            element: "#current-tree-vis-container",
             intro: "Otherwise, if your command does not solve the goal, you can look here.  This panel visualizes a directory containing the source code for a website.  More importantly, it shows how the directory's current status, or the output of your command, is different than the goal status or output.  Each non-empty directory can be expanded or collapsed by a single click. Click on the \"content\" directory and observe the effect.",
             position: "left"
         },
         {
-            element: "#bash-terminal",
+            element: "#bash-container",
             intro: "The visualization is updated after each command.  Type \"rm index.html\" in the terminal (without the quotes) and observe the effect.",
             position: "right"
         },
         {
-            element: "#current-tree-vis",
+            element: "#current-tree-vis-container",
             intro: "The icon of \"index.html\" turned grey, indicating that it has been deleted from your file system",
             position: "left"
         },
         {
-            element: "#bash-terminal",
+            element: "#bash-container",
             intro: "If a command outputs file names, the listed files are also highlighted in the visualization. Type \"find css/\" in the terminal and observe the effect.",
             position: "right"
         },
         {
-            element: "#current-tree-vis",
+            element: "#current-tree-vis-container",
             intro: "You have just selected all files and directories under the \"css\" directory. The files highlighted in yellow are in the desired output. The two directories are highlighted in red, since they are not in the desired output.",
             position: "left"
         },
