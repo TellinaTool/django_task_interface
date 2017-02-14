8c0a9f8fd1bda57ed33a5659e536d1c605ee3f6a
Victoria Lin <xilin@cs.washington.edu>
2017-02-13 23:56:57 -0800

    compute completion rate

--- scripts/create_table.py
+++ scripts/create_table.py
@@ -17,17 +17,21 @@ def create_stat_table(output_path):
                     status='finished').order_by('creation_time'):
                     finished_study_session = study_session
                 stage_i_total_time = finished_study_session.stage_total_time_spent('I')
+                completion_rate = finished_study_session.stage_completion_rate('I')
                 task = '0' if finished_study_session.task_block_order == '0' else '1'
                 treatment = 'A' if finished_study_session.treatment_order == '0' else 'B'
                 order = 'I'
                 o_f.write('{},{},{},{},{}\n'.format(stage_i_total_time.seconds,
+                                                    completion_rate,
                                                     user.access_code,
                                                     task, treatment, order))
                 stage_ii_total_time = finished_study_session.stage_total_time_spent('II')
+                completion_rate = finished_study_session.stage_completion_rate('II')
                 task = '1' if finished_study_session.task_block_order == '0' else '0'
                 treatment = 'B' if finished_study_session.treatment_order == '0' else 'A'
                 order = 'II'
                 o_f.write('{},{},{},{},{}\n'.format(stage_ii_total_time.seconds,
+                                                    completion_rate,
                                                     user.access_code,
                                                     task, treatment, order))
         o_f.close()
